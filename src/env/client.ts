2020f5a8b8245938708a998eb417ee7d35f4fbea
Mohammed Zaghloul <m.salahz86@gmail.com>
2025-11-29 18:27:19 +0200

    feat: Separate client and server environment configurations
    - Renamed `env.ts` to `client.ts` and updated the export to `cenv`.
    - Created a new `server.ts` file for server-specific environment configurations.
    - Added validation for `DATABASE_URL` in the server environment.
    - Introduced `emptyStringAsUndefined` option to handle empty string values.

--- src/env/client.ts
+++ src/env/client.ts
@@ -1,20 +1,13 @@
 import { createEnv } from '@t3-oss/env-core'
-import { z } from 'zod'
 
-export const env = createEnv({
-  server: {
-    SERVER_URL: z.string().url().optional(),
-  },
-
+export const cenv = createEnv({
   /**
    * The prefix that client-side variables must have. This is enforced both at
    * a type-level and at runtime.
    */
   clientPrefix: 'VITE_',
 
-  client: {
-    VITE_APP_TITLE: z.string().min(1).optional(),
-  },
+  client: {},
 
   /**
    * What object holds the environment variables at runtime. This is usually
